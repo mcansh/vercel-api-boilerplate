65d876e960003d0267bdc60635287fee93e7a93c
Logan McAnsh <logan@mcan.sh>
2020-10-26 21:08:25 -0400

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import type { NowApiHandler } from "@vercel/node";
+
+const handler: NowApiHandler = (req, res) => {
+  return res.status(200).end(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello World!</title>
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+    </html>
+  `);
+};
+
+export default handler;
